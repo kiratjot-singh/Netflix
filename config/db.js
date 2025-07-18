ea5360f0b303e8ea1af218410576560f1e05209c
Kirat <sandhukirat482@gmail.com>
2025-07-18 14:52:37 +0530

    Your changes

--- config/db.js
+++ config/db.js
@@ -1,6 +1,14 @@
-const mongoose=require('mongoose')
-const connection=mongoose.connect("mongodb://0.0.0.0/netflix").then(()=>{
-    console.log("connected");
+// const mongoose=require('mongoose')
+// const connection=mongoose.connect("mongodb://0.0.0.0/netflix").then(()=>{
+//     console.log("connected");
     
+// })
+// module.exports=connection
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-module.exports=connection
+.then(() => console.log("✅ MongoDB Connected"))
+.catch((err) => console.error("❌ MongoDB Connection Error:", err));
